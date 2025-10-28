d4b8d528970c8164080f907d6ecbe2b81cbd8b2a
Andikarya17 <andikaryaputra17@gmail.com>
2025-10-28 09:54:50 +0700

    nisialisasi server Express.js dan koneksi Sequelize, menambahkan middleware express.json() dan express.urlencoded() untuk menangani request body JSON dan form.

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded(
+    {
+        extended: true
+    }
+));
